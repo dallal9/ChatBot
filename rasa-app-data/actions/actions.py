f4b1876870714e1f38aade5e6534489fc988d792
tidaren <tidaren@tidaren.tidaren>
2019-05-28 21:40:41 +0200

    Add action to get first name and last name from facebook

--- rasa-app-data/actions/actions.py
+++ rasa-app-data/actions/actions.py
@@ -7,9 +7,30 @@ from __future__ import unicode_literals
 import logging
 import requests
 import json
+import yaml
 from rasa_core_sdk import Action
+from rasa_core_sdk.events import SlotSet
 
 logger = logging.getLogger(__name__)
+
+
+with open("config/credentials.yml") as f:
+    credentials = yaml.safe_load(f)
+
+
+class ActionMessengerName(Action):
+    def name(self):
+        return "action_messenger_name"
+
+    def run(self, dispatcher, tracker, domain):
+        most_recent_state = tracker.current_state()
+        sender_id = most_recent_state["sender_id"]
+        fb_access_token = credentials["facebook"]["page-access-token"]
+
+        r = requests.get("https://graph.facebook.com/{}?fields=first_name,last_name,profile_pic&access_token={}".format(sender_id, fb_access_token)).json()
+        first_name = r["first_name"]
+        last_name = r["last_name"]
+        return [SlotSet("first_name", first_name), SlotSet("last_name", last_name)]
 
 
 class ActionJoke(Action):
